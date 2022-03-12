92fc17b76fdc8e7d19abfa7c5291e3eb77906343
Andrew Kessinger <andrew.kessinger@gmail.com>
2022-03-12 13:43:58 +0100

    V1

--- components/Results.js
+++ components/Results.js
@@ -14,7 +14,7 @@ const Results = () => {
 
             <p>Current answers: {answers.length}</p>
             <br /><br />
-            <Link href="/index" passHref>
+            <Link href="/" passHref>
                 <button
                     type="button"
                     className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
